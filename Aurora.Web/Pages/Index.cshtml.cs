8959665eb0bff609abfdeae844d4851b0df2dd00
Ignacio Tampe <Igtampe@gmail.com>
2021-07-28 21:15:15 -0400

    User Friendly Error Handling

--- Aurora.Web/Pages/Index.cshtml.cs
+++ Aurora.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.IO;
 
@@ -9,17 +10,22 @@ namespace Igtampe.Aurora.Web.Pages {
 
         public TimeSpan Uptime { get { try { return DateTime.Now - Collection.LastOutage.End; } catch (Exception) { return new TimeSpan(0); } } }
         
-        public void OnGet() {
+        public IActionResult OnGet() {
 
             if (!System.IO.File.Exists("Path.txt")) {
                 System.IO.File.WriteAllText("Path.txt", "Put a path here!");
-                throw new FileNotFoundException("Was unable to find Path.txt, A temporary file has been created. Put the path to the AURLOG to display here.");
+                return Redirect("./NotConfigured");
             }
 
             string AurLogLocation = System.IO.File.ReadAllText("Path.txt").Replace("\"","");
-            if (!System.IO.File.Exists(AurLogLocation)) { throw new FileNotFoundException($"Was unable to find {AurLogLocation}.");  }
+            if (AurLogLocation == "Put a path here!") { return Redirect("./NotConfigured"); }
+            if (!System.IO.File.Exists(AurLogLocation)) { return Redirect("./NotFound"); }
 
-            Collection = OutageCollection.LoadOutageCollection(AurLogLocation,new TimeSpan(30,0,0,0,0));
+            Collection = OutageCollection.LoadOutageCollection(AurLogLocation,new TimeSpan(365,0,0,0,0));
+
+            if (Collection.Count == 0) { return Redirect("./empty"); }
+
+            return null;
         }
     }
 }
